10ebce51e405bcf30496a27aa9040e4f185524a0
Thomas Chang <thc005@ucsd.edu>
2014-10-16 22:06:34 -0700

    2

--- app/routes.js
+++ app/routes.js
@@ -8,7 +8,10 @@ module.exports = function(app, passport) {
 
     app.get('/public', function(req, res){
         User.find({type: 'user'}, function(err,users){
-            if(err) return console.error(err);
+            if(err) {
+                res.send("There was an error. Please report this error code to primetime: 0")
+                return console.error(err);
+            }
 
             var retArray = new Array();
             users.forEach(function(user){
@@ -22,77 +25,109 @@ module.exports = function(app, passport) {
 
     app.get('/', function(req, res){
        Item.find({active:true}, function(err, items){
+           if(err) {
+               res.send("There was an error. Please report this error code to primetime: 1")
+               return console.error(err);
+           }
            res.render('members.ejs', {
-              items: items
+              items: items,
+               message: ''
            });
        })
     });
 
+    function endBuy(message, res){
+        Item.find({active:true}, function(err, items){
+            res.render('members.ejs', {
+                items: items,
+                message: message
+            });
+        })
+    }
     app.post('/buystore', function(req,res){
-
         if(!validator.isUUID(req.body.UUID)){
-            res.send("Not a valid passcode.")
+            endBuy("Not a valid passcode.", res);
             return;
         }
 
+        console.log("Request made with UUID: " + req.body.UUID + " and item name: " + req.body.name);
+
         User.findOne({UUID: req.body.UUID}, function(err, user){
             if(err){
-                res.end("There was an error, try again later.");
+                endBuy("There was an error. Please report this error code to primetime: 2", res);
                 throw err;
                 return;
             }
             if(user){
                 Item.findOne({name:req.body.name}, function(err, item){
                     if(err){
-                        res.end("There was an error, try again later.");
+                        endBuy("There was an error, try again later.", res);
                         throw err;
                         return;
                     }
                     if(item) {
-                        if (user.ccp >= item.ccp && item.active) {
+                        if (user.ccp >= item.ccp && item.active && item.quantity > 0) {
                             Changelog.findOne({type: 'store'}, function (err, changelog) {
                                 if (err) {
-                                    res.end("There was an error, try again later.");
+                                    endBuy("There was an error. Please report this error code to primetime: 3", res);
                                     throw err;
                                     return;
                                 }
                                 if (changelog) {
-                                    changelog.content += user.name + " bought a " + item.name + " for " + item.ccp + " ccp."
+                                    changelog.content += user.name + " bought a " + item.name + " for " + item.ccp + " ccp. CCP Change from " + user.ccp + " to " + user.ccp- item.ccp + ". Item Quantity Change from " + item.quantity + " to " + item.quantity-1;
                                     if (changelog.content.length > 100000) {
                                         if (changelog.content.indexOf('\n') != -1)
                                             changelog.content = changelog.content.substring(changelog.content.indexOf('\n'));
                                     }
-                                    changelog.save(function (err) {
-                                        if (err) {
-                                            res.end("There was an error, try again later.");
-                                            throw err;
-                                            return;
-                                        }
-                                        user.ccp = user.ccp - item.ccp;
-                                        user.save(function (err) {
+
+                                    user.ccp = user.ccp - item.ccp;
+                                    item.quantity = item.quantity - 1;
+
+                                        changelog.save(function (err) {
                                             if (err) {
-                                                res.end("There was an error, try again later.");
+                                                endBuy("There was an error. Please report this error code to primetime: 4", res);
                                                 throw err;
                                                 return;
                                             }
+                                            changelogFlag = true;
+                                        });
 
-                                            res.send("Success! Your ccp is now at " + user.ccp);
-                                            return;
-                                        })
-                                    })
+                                        user.save(function (err) {
+                                            if (err) {
+                                                endBuy("There was an error. Please report this error code to primetime: 5", res);
+                                                throw err;
+                                                return;
+                                            }
+                                            userFlag = true;
+                                        });
+
+                                        item.save(function (err) {
+                                            if (err) {
+                                                endBuy("There was an error. Please report this error code to primetime: 6", res);
+                                                throw err;
+                                                return;
+                                            }
+                                            itemFlag = true;
+                                        });
+
+                                    endBuy("Success! Your ccp is now at " + user.ccp, res);
+                                    return;
                                 }
+
                             })
+
                         } else {
-                            res.send("You do not have enough ccp or the item is no longer in the store.")
+                            endBuy("You do not have enough ccp or the item is no longer in the store.", res)
                             return;
+
                         }
                     } else {
-                        res.send("Item not found.");
+                        endBuy("Item not found.", res);
                         return;
                     }
                 })
             } else {
-                res.send("Passcode not recognized.")
+                endBuy("Passcode not recognized.", res)
                 return;
             }
         })
